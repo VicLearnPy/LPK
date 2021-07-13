3801324b6a68c48eccbd687763227bea858a4876
VicLearnPy <85521429+VicLearnPy@users.noreply.github.com>
2021-07-13 19:29:17 +0200

    Add files via upload

---
+++ Finder.py
@@ -0,0 +1,56 @@
+import colorama
+import time 
+import colorama
+from colorama import Fore
+import os
+
+def close(): 
+    input("""
+[LKP]Appuyez sur une touche pour fermer le programme ...""")
+
+def clear():
+     os.system('cls')
+
+def graph(): 
+        colorama.init()
+        print(Fore.BLUE +"""  
+                         
+     _______ _______ _______ 
+    |\     /|\     /|\     /|
+    | +---+ | +---+ | +---+ |
+    | |   | | |   | | |   | |
+    | |L  | | |K  | | |P  | |
+    | +---+ | +---+ | +---+ |
+    |/_____\|/_____\|/_____\|
+                         
+""")
+
+graph()
+
+F1 = input(str("[LKP] The path of the file : "))
+Cr = input("[LKP] The word to find : ")
+file = open(F1, 'r')
+file.close()
+
+def check():
+            with open(F1,) as f:
+                if Cr in f.read():
+                    print("""
+[LKP] Yes there is  """)
+#                 N = file.count(Cr)
+#                    print(Fore.BLUE +""" 
+#[LKP] Le mot apparait """ ,N ,Fore.BLUE +"Fois")
+                else : 
+                    print("""
+[LKP] No is not here""")
+
+
+def wait(): 
+    clear()
+    graph()
+    print (str(Fore.BLUE +("[LKP] Finding ...")))
+    time.sleep(5)
+
+wait()
+check()
+close()
